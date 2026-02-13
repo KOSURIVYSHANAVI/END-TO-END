f316fb2d0fc41b5f1ff8b4f96109e0975f1677c2
Kosuri Vyshanavi <kosurivyshnavi2006@gmail.com>
2026-02-13 23:18:33 +0530

    Setup backend & frontend integration for Module 3

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,12 @@
-from flask import Flask
-from flask_cors import CORS
+from flask import Flask, jsonify
+from flask_cors import CORS  # Allow React frontend to access Flask
 
 app = Flask(__name__)
-CORS(app)
+CORS(app)  # This is very important for React requests
 
 @app.route("/")
 def home():
-    return "Placement Readiness Backend Running"
+    return jsonify({"message": "Backend Connected Successfully!"})
 
 if __name__ == "__main__":
     app.run(debug=True)
