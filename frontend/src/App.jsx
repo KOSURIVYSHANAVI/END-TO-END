2f911a388133aa51a52bb2f94bf6bf40db62ca82
Kosuri Vyshanavi <kosurivyshnavi2006@gmail.com>
2026-02-18 19:04:41 +0530

    Worked on homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,15 @@
-import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  const [students, setStudents] = useState([]);
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:5000/students")
-      .then((res) => res.json())
-      .then((data) => setStudents(data))
-      .catch((err) => console.error("Error:", err));
-  }, []);
-
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Placement Readiness Dashboard</h1>
-
-      {students.map((student) => (
-        <div key={student.id} style={{ marginBottom: "20px" }}>
-          <h3>{student.name}</h3>
-
-          <div
-            style={{
-              width: "300px",
-              backgroundColor: "#ddd",
-              borderRadius: "10px",
-            }}
-          >
-            <div
-              style={{
-                width: `${student.progress}%`,
-                backgroundColor: "green",
-                padding: "5px",
-                borderRadius: "10px",
-                color: "white",
-                textAlign: "center",
-              }}
-            >
-              {student.progress}%
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
